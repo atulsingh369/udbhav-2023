6ca59e32c23cbfcd292570db8e4b04ede6a1fea6
Abhishek Kant <prajapati27092002@gmail.com>
2023-04-02 19:27:58 +0530

    initial commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYOyKkZwmCiant4WQYiRNqN9AiC87pqA8",
+  authDomain: "udbhav-22dbd.firebaseapp.com",
+  projectId: "udbhav-22dbd",
+  storageBucket: "udbhav-22dbd.appspot.com",
+  messagingSenderId: "334534691997",
+  appId: "1:334534691997:web:ae3a9588c39be95eb7d874",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
