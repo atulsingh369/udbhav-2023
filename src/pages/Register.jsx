033223a9c675c4d1657ca63a0ce953853ee46200
atulsingh369 <businessatul7684@gmail.com>
2023-04-06 11:56:34 +0530

    google Login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import GoogleButton from "react-google-button";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   updateProfile,
-  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 import { auth } from "../config";
 import { useDispatch } from "react-redux";
@@ -13,6 +15,7 @@ import { useNavigate } from "react-router-dom";
 const Register = () => {
   const [state, setState] = useState(false);
   const [loading, setLoading] = useState(false);
+  const provider = new GoogleAuthProvider();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [curUser, setCurUser] = useState({
@@ -64,83 +67,32 @@ const Register = () => {
         });
       })
       .catch((error) => {
-				console.log(error);
-				window.alert(error);
+        console.log(error);
+        window.alert(error);
       });
   };
 
-  // //Form Registerd
-  // const postData = async (event) => {
-  //   event.preventDefault();
-
-  //   const { name, email, password } = curUser;
-
-  //   if (name && email && password) {
-  //     const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
-  //     if (regEx.test(email)) {
-  //       const res = await fetch(
-  //         "https://udbhav-22dbd-default-rtdb.firebaseio.com/Udbhav2k23_users.json",
-  //         {
-  //           method: "POST",
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //           },
-  //           body: JSON.stringify({
-  //             name,
-  //             email,
-  //             password,
-  //           }),
-  //         }
-  //       );
-
-  //       createUserWithEmailAndPassword(auth, email, password)
-  //         .then((userCredential) => {
-  //           // Signed in
-  //           const curUser = userCredential.curUser;
-  //           console.log(curUser);
-  //         })
-  //         .catch((error) => {
-  //           const errorCode = error.code;
-  //           const errorMessage = error.message;
-  //           console.log(errorCode, errorMessage);
-  //         });
-
-  //       //Empting the column
-  //       if (res) {
-  //         setCurUser({
-  //           name: "",
-  //           email: "",
-  //           password: "",
-  //         });
-  //       }
-
-  //       alert("Data Stored");
-  //     } else {
-  //       alert("Enter Correct Email");
-  //     }
-  //   } else {
-  //     alert("please fill all Columns");
-  //   }
-  // };
-
-  // //Login Done
-  // const getData = async (e) => {
-  //   e.preventDefault();
-
-  //   const res = await fetch(
-  //     "https://udbhav-22dbd-default-rtdb.firebaseio.com/Udbhav2k23_users.json"
-  //   );
-
-  //   if (res) {
-  //     setCurUser({
-  //       name: "",
-  //       email: "",
-  //       password: "",
-  //     });
-  //   }
-
-  //   console.log("Login");
-  // };
+  const googleLogin = async () => {
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        console.log(error);
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
 
   //Form Display
   //true --> log in page
@@ -148,127 +100,133 @@ const Register = () => {
   const changeState = () => setState(!state);
   return (
     <div className="h-screen pt-20 flex justify-center items-center bg-form-background">
-      {!state && (
-        //Register Page
+      <div className="bg-white shadow-2xl rounded-lg shadow-blue-800 md:h-auto h-screen md:w-96 w-screen p-5">
+        {!state && (
+          //Register Page
+          <div className="flex flex-col items-center justify-evenly gap-7">
+            <p className="font-bold  text-3xl ">Register</p>
 
-        <div className="bg-white shadow-2xl rounded-lg shadow-blue-800 md:h-auto h-screen md:w-96 w-screen flex flex-col items-center justify-evenly gap-7 p-5">
-          <p className="font-bold  text-3xl ">Register</p>
+            <div className="form-control w-full max-w-xs">
+              <input
+                name="name"
+                autoFocus
+                value={curUser.name}
+                onChange={(e) =>
+                  setCurUser({ ...curUser, name: e.target.value })
+                }
+                type="text"
+                placeholder="Username"
+                autoComplete="off"
+                required
+                className="input bg-white border border-base-100 shadow-lg   input-bordered w-full max-w-xs"
+              />
+            </div>
 
-          <div className="form-control w-full max-w-xs">
+            <div className="form-control w-full max-w-xs">
+              <input
+                type="text"
+                name="email"
+                value={curUser.email}
+                onChange={(e) =>
+                  setCurUser({ ...curUser, email: e.target.value })
+                }
+                placeholder="Email"
+                autoComplete="off"
+                required
+                className="input bg-white border border-base-100 shadow-lg  input-bordered w-full max-w-xs "
+              />
+            </div>
+
+            <div className="form-control w-full max-w-xs">
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={curUser.password}
+                onChange={(e) =>
+                  setCurUser({ ...curUser, password: e.target.value })
+                }
+                autoComplete="off"
+                required
+                className="input bg-white border border-base-100 shadow-lg  input-bordered w-full max-w-xs"
+              />
+            </div>
+
             <input
-              name="name"
-              autoFocus
-              value={curUser.name}
-              onChange={(e) => setCurUser({ ...curUser, name: e.target.value })}
-              type="text"
-              placeholder="Username"
-              autoComplete="off"
-              required
-              className="input bg-white border border-base-100 shadow-lg   input-bordered w-full max-w-xs"
+              value="Register"
+              type={loading ? "Registering..." : "Register"}
+              className="btn"
+              onClick={() => signUp()}
             />
-          </div>
 
-          <div className="form-control w-full max-w-xs">
+            <p>
+              Already registered? &nbsp;
+              <span
+                onClick={changeState}
+                className="cursor-pointer text-blue-500">
+                Sign In
+              </span>
+            </p>
+          </div>
+        )}
+        {state && (
+          // Login Page
+          <div className="flex flex-col items-center justify-evenly gap-7">
+            <p className="font-bold  text-3xl ">Log In</p>
+
+            <div className="form-control w-full max-w-xs">
+              <input
+                autoFocus
+                type="text"
+                name="email"
+                value={curUser.email}
+                onChange={(e) =>
+                  setCurUser({ ...curUser, email: e.target.value })
+                }
+                placeholder="Email"
+                autoComplete="off"
+                required
+                className="input bg-white border border-base-100 shadow-lg  input-bordered w-full max-w-xs "
+              />
+            </div>
+
+            <div className="form-control w-full max-w-xs">
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={curUser.password}
+                onChange={(e) =>
+                  setCurUser({ ...curUser, password: e.target.value })
+                }
+                autoComplete="off"
+                required
+                className="input bg-white border border-base-100 shadow-lg  input-bordered w-full max-w-xs"
+              />
+            </div>
+
             <input
-              type="text"
-              name="email"
-              value={curUser.email}
-              onChange={(e) =>
-                setCurUser({ ...curUser, email: e.target.value })
-              }
-              placeholder="Email"
-              autoComplete="off"
-              required
-              className="input bg-white border border-base-100 shadow-lg  input-bordered w-full max-w-xs "
+              value="Log In"
+              type="submit"
+              className="btn "
+              onClick={() => signIn()}
             />
+            <p>
+              Don't have an account?&nbsp;
+              <span
+                onClick={changeState}
+                className="cursor-pointer text-blue-500">
+                &nbsp;Register
+              </span>
+            </p>
           </div>
-
-          <div className="form-control w-full max-w-xs">
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={curUser.password}
-              onChange={(e) =>
-                setCurUser({ ...curUser, password: e.target.value })
-              }
-              autoComplete="off"
-              required
-              className="input bg-white border border-base-100 shadow-lg  input-bordered w-full max-w-xs"
-            />
-          </div>
-
-          <input
-            value="Register"
-            type={loading ? "Registering..." : "Register"}
-            className="btn"
-            onClick={() => signUp()}
-          />
-
-          <p>
-            Already registered? &nbsp;
-            <span
-              onClick={changeState}
-              className="cursor-pointer text-blue-500">
-              Sign In
-            </span>
-          </p>
+        )}
+        ;
+        <div className="flex flex-col justify-center items-center">
+          <p className="mb-4">OR</p>
+          <GoogleButton onClick={googleLogin} />
         </div>
-      )}
-      {state && (
-        // Login Page
-
-        <div className="bg-white shadow-2xl rounded-lg shadow-blue-800 md:h-auto h-screen md:w-96 w-screen flex flex-col items-center justify-evenly gap-7 p-5">
-          <p className="font-bold  text-3xl ">Log In</p>
-
-          <div className="form-control w-full max-w-xs">
-            <input
-              autoFocus
-              type="text"
-              name="email"
-              value={curUser.email}
-              onChange={(e) =>
-                setCurUser({ ...curUser, email: e.target.value })
-              }
-              placeholder="Email"
-              autoComplete="off"
-              required
-              className="input bg-white border border-base-100 shadow-lg  input-bordered w-full max-w-xs "
-            />
-          </div>
-
-          <div className="form-control w-full max-w-xs">
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={curUser.password}
-              onChange={(e) =>
-                setCurUser({ ...curUser, password: e.target.value })
-              }
-              autoComplete="off"
-              required
-              className="input bg-white border border-base-100 shadow-lg  input-bordered w-full max-w-xs"
-            />
-          </div>
-
-          <input
-            value="Log In"
-            type="submit"
-            className="btn "
-            onClick={() => signIn()}
-          />
-          <p>
-            Don't have an account?&nbsp;
-            <span
-              onClick={changeState}
-              className="cursor-pointer text-blue-500">
-              &nbsp;Register
-            </span>
-          </p>
-        </div>
-      )}
-      ;
+      </div>
     </div>
   );
 };
