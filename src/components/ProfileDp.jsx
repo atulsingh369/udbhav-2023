6cfc2311ab22f13fb6ea942f1a80434110db5338
Abhishek Kant <prajapati27092002@gmail.com>
2023-04-07 12:53:38 +0530

    profile page updated

--- src/components/ProfileDp.jsx
+++ src/components/ProfileDp.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import { useState } from "react";
 import { IoMdAddCircle } from "react-icons/io";
 const ProfileDp = () => {
+  const [edit, setEdit] = useState(false);
   return (
-    <div>
-      <div className="avatar w-fit flex flex-col items-end">
+    <div className="bg-base-100 pt-20">
+      <div className="avatar w-fit flex flex-col items-end ">
         <div className="w-56 rounded-full ">
           <img src="https://ik.imagekit.io/e5ixuxrlb/Udbhav/dp.png?updatedAt=1680849763188" />
         </div>
-        <IoMdAddCircle className="text-6xl text-white -translate-y-16" />
+        <IoMdAddCircle
+          onClick={() => setEdit(!edit)}
+          className="text-6xl text-white  -translate-y-16"
+        />
+        {edit && (
+          <div>
+            <ul>
+              <li className="flex flex-col absolute  -translate-y-24 gap-5">
+                <a className="btn">Edit DP</a>
+                <a className="btn">Edit Profile</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const ProfileInfo = () => {
   return (
-    <div className="flex flex-col justify-evenly text-white bg-base-100 ">
+    <div className="flex flex-col justify-evenly text-white md:pt-20 bg-base-100 items-center -mt-5 gap-5">
       <div>
         <h1 className="text-3xl font-semibold">Abhishek Kant</h1>
         <h1 className="text-1xl font-bold">User's UDBHAV ID</h1>
       </div>
       <div>
-        <h1 className="text-2xl ">Branch</h1>
-        <h1 className="text-2xl ">Batch</h1>
-        <h1 className="text-2xl ">Number of Events Participated</h1>
+        <h1 className="text-lg md:text-2xl ">Branch</h1>
+        <h1 className="text-lg md:text-2xl ">Batch</h1>
+        <h1 className="text-lg md:text-2xl ">Number of Events Participated</h1>
       </div>
     </div>
   );
