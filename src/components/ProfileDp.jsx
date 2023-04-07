2c0ed1d214469654c0b643bd8c76c766dff66f47
atulsingh369 <businessatul7684@gmail.com>
2023-04-07 12:40:28 +0530

    profile pic updated

--- src/components/ProfileDp.jsx
+++ src/components/ProfileDp.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { IoMdAddCircle } from "react-icons/io";
+import { useSelector } from "react-redux";
+
 const ProfileDp = () => {
+  const user = useSelector((state) => state.user);
   return (
     <div>
       <div className="avatar w-fit flex flex-col items-end">
         <div className="w-56 rounded-full ">
-          <img src="https://ik.imagekit.io/e5ixuxrlb/Udbhav/dp.png?updatedAt=1680849763188" />
+          {user.photoURL && <img src={user.photoURL} />}
+          {!user.photoURL && (
+            <img src="https://ik.imagekit.io/xji6otwwkb/Profile.png?updatedAt=1680849745697" />
+          )}
         </div>
         <IoMdAddCircle className="text-6xl text-white -translate-y-16" />
       </div>
