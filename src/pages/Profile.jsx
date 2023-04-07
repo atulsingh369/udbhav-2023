4f14e1ba5b54c3bacb12da433955c317264621f8
Abhishek Kant <prajapati27092002@gmail.com>
2023-04-07 13:06:19 +0530

    DONE CHANGWES

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,12 +4,12 @@ import ProfileInfo from "../components/ProfileInfo";
 
 const Profile = () => {
   return (
-    <div className="h-screen  bg-white justify-evenly">
-      <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center bg-base-100">
-        <ProfileDp />
-        <ProfileInfo />
-      </div>
-    </div>
+    // <div className="h-screen  bg-white justify-evenly">
+    //   <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center bg-base-100">
+    //     <ProfileDp />
+    //     <ProfileInfo />
+    //   </div>
+    // </div>
   );
 };
 
