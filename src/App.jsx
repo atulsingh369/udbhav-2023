09b78de62b3f6ddd1b15a850a7c659f39854fca5
Rohit <rk22012001@gmail.com>
2023-04-12 15:56:51 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -12,48 +12,32 @@ import Landing from "./components/Landing";
 import TechForm from "./components/forms/techForms";
 import CultForm from "./components/forms/cultForms";
 import EasterEgg from "./components/EasterEgg";
-import { useEffect, useState } from "react";
-import MainLoader from "./components/MainLoader";
 
 const App = () => {
-  const [load, setLoad] = useState(false);
-  useEffect(() => {
-    setLoad(true);
-    setTimeout(() => {
-      setLoad(false);
-    }, 8000);
-  }, []);
+  z;
   return (
-    <>
-      {load ? (
-        <div className="grid place-items-center h-screen text-black text-3xl">
-          <MainLoader />
-        </div>
-      ) : (
-        <div className="relative">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/tour" element={<Tour />} />
-            {/* <Route path="/forms" element={<Forms />} /> */}
-            <Route path="/events" element={<Events />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-            <Route path="/register" element={<Register />} />
-            {/* events pages routing */}
-            <Route path="/technovation" element={<TechEventsPage />} />
-            <Route path="/cultural" element={<CulturalEventPage />} />
-            {/* Form pages */}
-            <Route path="/techform/:id" element={<TechForm />} />
-            <Route path="/culturalform/:id" element={<CultForm />} />
-            {/* Profile page */}
-            <Route path="/profile" element={<Profile />} />
-            {/* hidden eatser */}
-            <Route path="/hidden-easter" element={<EasterEgg />} />
-            {/* Loader */}
-          </Routes>
-        </div>
-      )}
-    </>
+    <div className="relative">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/tour" element={<Tour />} />
+        {/* <Route path="/forms" element={<Forms />} /> */}
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/register" element={<Register />} />
+        {/* events pages routing */}
+        <Route path="/technovation" element={<TechEventsPage />} />
+        <Route path="/cultural" element={<CulturalEventPage />} />
+        {/* Form pages */}
+        <Route path="/techform/:id" element={<TechForm />} />
+        <Route path="/culturalform/:id" element={<CultForm />} />
+        {/* Profile page */}
+        <Route path="/profile" element={<Profile />} />
+        {/* hidden eatser */}
+        <Route path="/hidden-easter" element={<EasterEgg />} />
+        {/* Loader */}
+      </Routes>
+    </div>
   );
 };
 
