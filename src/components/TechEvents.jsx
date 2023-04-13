6f05386336b388b6b0b1dd0ba1b2d265ac13ea79
Abhishek Kant <prajapati27092002@gmail.com>
2023-04-13 18:24:10 +0530

    sponsers section added

--- src/components/TechEvents.jsx
+++ src/components/TechEvents.jsx
@@ -3,17 +3,18 @@ import { techEvents } from "./Data";
 import { useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import SponsersGallery from "./SponsersGallery";
 
 const TechEvents = () => {
   const user = useSelector((state) => state.user);
 
   const alert = () => {
     toast.warning("Forms will be available soon");
-	};
-	
-		const login = () => {
-      toast.warning("Login to Continue");
-    };
+  };
+
+  const login = () => {
+    toast.warning("Login to Continue");
+  };
 
   return (
     <div className="flex flex-col items-center justify-center mt-14 lg:mt-0 ">
@@ -22,13 +23,15 @@ const TechEvents = () => {
           TECHNOVATION
         </h1>
       </div>
+      <SponsersGallery />
       {user && (
         <div className=" bg-home-background bg-cover flex flex-row flex-wrap justify-center h-max">
           {Object.entries(techEvents).map((item, i) => {
             return (
               <div
                 className="flex flex-col gap-8 justify-center items-center p-8 cursor-pointer hover:scale-105 transition-all ease-in-out duration-300"
-                key={i}>
+                key={i}
+              >
                 <div className="flex gap-5 flex-wrap justify-center items-center">
                   {item[1].map((value, index) => {
                     return (
@@ -36,7 +39,8 @@ const TechEvents = () => {
                       <div
                         onClick={alert}
                         className="card w-64 h-[28rem] bg-base-100 shadow-xl"
-                        key={index}>
+                        key={index}
+                      >
                         <figure>
                           <img
                             src={value.image}
@@ -66,14 +70,16 @@ const TechEvents = () => {
             return (
               <div
                 className="flex flex-col gap-8 justify-center items-center p-8 cursor-pointer hover:scale-105 transition-all ease-in-out duration-300"
-                key={i}>
+                key={i}
+              >
                 <div className="flex gap-5 flex-wrap justify-center items-center">
                   {item[1].map((value, index) => {
                     return (
-											<div
-												onClick={login}
+                      <div
+                        onClick={login}
                         className="card w-64 h-[28rem] bg-base-100 shadow-xl"
-                        key={index}>
+                        key={index}
+                      >
                         <figure>
                           <img
                             src={value.image}
