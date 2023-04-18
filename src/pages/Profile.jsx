454da17a8996c8764b79e2ddc45d47c88a0a852a
Abhishek Kant <prajapati27092002@gmail.com>
2023-04-19 02:37:35 +0530

    .

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,26 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ProfileDp from "../components/ProfileDp";
 import ProfileInfo from "../components/ProfileInfo";
 import RegisteredEvents from "../components/RegisteredEvents";
 import { db } from "../config";
-import { collection, getDocs } from "firebase/firestore";
-import { useNavigate } from "react-router-dom";
+// import { collection, getDocs } from "firebase/firestore";
 
 const Profile = () => {
-  const navigate = useNavigate()
-  // useEffect(() => {
-  //   const handleBeforeUnload = (event) => {
-  //     // Do something when the beforeunload event is triggered
-  //     console.log("object")
-  //     // navigate("/forms")
-  //   };
 
-  //   // window.addEventListener('beforeunload', navigate("/"));
-
-  //   return () => {
-  //     window.removeEventListener('beforeunload', navigate("/"));
-  //   };
-  // }, []);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-home-background bg-cover sticky h-screen">
       <ProfileDp />
