25928a9b6eece9ba63d344807855caa9fe3da127
atulsingh369 <businessatul7684@gmail.com>
2023-04-21 10:03:33 +0530

    Updated email in form

--- src/components/forms/cultForms.jsx
+++ src/components/forms/cultForms.jsx
@@ -17,7 +17,7 @@ const cultForms = () => {
 
   const initialValues = {
     mName: "",
-    branch: "",
+    email: "",
     phnNo: "",
   };
 
@@ -26,7 +26,7 @@ const cultForms = () => {
 
   const addMember = (e) => {
     e.preventDefault();
-    if (!values.mName || !values.branch || !values.phnNo) {
+    if (!values.mName || !values.email || !values.phnNo) {
       // if (condition) {
 
       // }
@@ -74,7 +74,7 @@ const cultForms = () => {
   const submitSolo = async (e) => {
     e.preventDefault();
     setLoading(true);
-    if (!values.mName || !values.branch || !values.phnNo) {
+    if (!values.mName || !values.email || !values.phnNo) {
       toast.error("Enter Details");
       setLoading(false);
       setValues(initialValues);
@@ -82,7 +82,7 @@ const cultForms = () => {
     } else {
       await setDoc(doc(db, id, values.email), {
         Name: values.mName,
-        Branch: values.branch,
+        Email: values.email,
         "Phone No": values.phnNo,
       });
       toast.success("Submitted");
@@ -99,12 +99,12 @@ const cultForms = () => {
       {Object.entries(cultEvents).map((item, i) => {
         if (item[0] === id) {
           return (
-            <div className="grid grid-cols-1
+            <div
+              className="grid grid-cols-1
              md:grid-cols-3 h-screen w-full   ">
               <div
                 className="login-box scrollbar-hidden w-full md:w-4/5 pt-20 md:pt-5 overflow-scroll h-screen md:h-fit mt-4 relative md:col-span-2"
-                key={i}
-              >
+                key={i}>
                 {item[1].map((value, index) => {
                   return (
                     <div key={index}>
@@ -127,8 +127,7 @@ const cultForms = () => {
                               return (
                                 <div
                                   className="flex flex-col md:flex-row gap-10"
-                                  key={i}
-                                >
+                                  key={i}>
                                   <div className="user-box">
                                     <input
                                       contentEditable={false}
@@ -139,12 +138,11 @@ const cultForms = () => {
                                   </div>
                                   <div className="user-box">
                                     <input
-                                      placeholder="CSE/A3"
                                       contentEditable={false}
-                                      type="text"
-                                      value={arr_item && arr_item.branch}
+                                      type="email"
+                                      value={arr_item && arr_item.email}
                                     />
-                                    <label>Branch/Year*</label>
+                                    <label>Email*</label>
                                   </div>
                                   <div className="user-box">
                                     <input
@@ -183,16 +181,15 @@ const cultForms = () => {
                           </div>
                           <div className="user-box">
                             <input
-                              placeholder="CSE/A3"
-                              type="text"
-                              value={values && values.branch}
+                              type="email"
+                              value={values && values.email}
                               onChange={(e) =>
-                                setValues({ ...values, branch: e.target.value })
+                                setValues({ ...values, email: e.target.value })
                               }
-                              name="branch"
+                              name="email"
                               required=""
                             />
-                            <label>Branch/Year*</label>
+                            <label>Email*</label>
                           </div>
                           <div className="user-box">
                             <input
@@ -213,8 +210,7 @@ const cultForms = () => {
                             <button
                               type="button"
                               onClick={addMember}
-                              className="border-2 text-sm border-white text-white p-2 hover:text-green-600 hover:border-green-600 rounded-xl h-fit"
-                            >
+                              className="border-2 text-sm border-white text-white p-2 hover:text-green-600 hover:border-green-600 rounded-xl h-fit">
                               Add
                             </button>
                           )}
@@ -237,15 +233,15 @@ const cultForms = () => {
                           <div className="user-box">
                             <input
                               placeholder="CSE/A3"
-                              type="text"
-                              value={values.branch}
+                              type="email"
+                              value={values.email}
                               onChange={(e) =>
-                                setValues({ ...values, branch: e.target.value })
+                                setValues({ ...values, email: e.target.value })
                               }
-                              name="branch"
+                              name="email"
                               required=""
                             />
-                            <label>Branch/Year*</label>
+                            <label>Email*</label>
                           </div>
                           <div className="user-box">
                             <input
@@ -283,8 +279,7 @@ const cultForms = () => {
                             value.type === "group" ? handleSubmit : submitSolo
                           }
                           className="submit"
-                          type="submit"
-                        >
+                          type="submit">
                           <span></span>
                           <span></span>
                           <span></span>
@@ -329,12 +324,14 @@ const cultForms = () => {
                   );
                 })}
               </div>
-             
+
               <div className=" h-screen w-full p-5 pt-10 md:pt-24  text-white bg-[#101a26] md:rounded-lg md:shadow-lg md:shadow-black/80 flex flex-col gap-4 overflow-y-scroll">
-                <h3 className="font-bold text-3xl text-center underline">{item[1][0].title}</h3>
+                <h3 className="font-bold text-3xl text-center underline">
+                  {item[1][0].title}
+                </h3>
                 <ul className="list-disc space-y-1  pl-8 leading-8">
                   {item[1][0].rules.map((rule, num) => {
-                    return <li >{rule}</li>;
+                    return <li>{rule}</li>;
                   })}
                 </ul>
               </div>
